5cb202489d26945eb8db9f4418be99fc00b85212
sanskar-coder <77289661+sanskar-coder@users.noreply.github.com>
2021-05-10 15:13:27 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,27 @@
+class Launcher{
+constructor(body,anchor){
+ var options={bodyA:body,pointB:anchor,stiffness:0.004,length:3}
+ this.bodyA = body;
+ this.pointB = anchor;
+ this.launcher = Constraint.create(options);
+ World.add(world,this.launcher);
+}
+
+display(){
+    if(this.launcher.bodyA){
+    var pointA = this.bodyA.position;
+    var pointB = this.pointB;
+    strokeWeight(3);
+    line (pointA.x,pointA.y,pointB.x,pointB.y);
+    } 
+}
+
+fly(){
+    this.launcher.bodyA = null;
+}
+
+attach(body){
+this.launcher.bodyA = body;
+}
+
+}
